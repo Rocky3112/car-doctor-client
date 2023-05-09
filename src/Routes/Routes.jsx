117b6b99067255680873a12de27fbffa1ca77b3a
Rocky3112 <alomgirhossainrocky@gmail.com>
2023-05-10 02:01:14 +0600

    project setup done

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layouts/Main";
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children:[
+        {
+            path:"/",
+            // element:
+        }
+      ]
+    },
+  ]);
+  
+
+export default router;
